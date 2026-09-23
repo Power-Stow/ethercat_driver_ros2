e55c8d816ab09fe5ebf717793d425c8b98272219
Thomas Jespersen <tkj@powerstow.com>
2026-09-23 15:58:20 +0200

    Fix startup wrap tests

--- ethercat_generic_plugins/ethercat_generic_cia402_drive/test/test_generic_ec_cia402_drive.hpp
+++ ethercat_generic_plugins/ethercat_generic_cia402_drive/test/test_generic_ec_cia402_drive.hpp
@@ -41,7 +41,7 @@ class FriendEcCiA402Drive : public ethercat_generic_plugins::EcCiA402Drive
   FRIEND_TEST(EcCiA402DriveTest, JointOffsetCompensatesCspCommandPosition);
   FRIEND_TEST(EcCiA402DriveTest, JointOffsetStartupWrapDisabledKeepsLegacyBehavior);
   FRIEND_TEST(EcCiA402DriveTest, JointOffsetStartupWrapEnabledAdjustsFirstSampleOnly);
-  FRIEND_TEST(EcCiA402DriveTest, JointOffsetStartupWrapWaitsForOperationalState);
+  FRIEND_TEST(EcCiA402DriveTest, JointOffsetStartupWrapWaitsForSlaveData);
 };
 
 class EcCiA402DriveTest : public ::testing::Test
